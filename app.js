83908603da2fef074328f2e7e503a9eea71d6f51
Дастан <nuragzyuuludastan@gmail.com>
2024-11-23 10:05:53 +0600

    add project

---
+++ app.js
@@ -0,0 +1,26 @@
+const API = 'http://localhost:3000/product'
+
+async function fetchProduct() {
+	const response = await fetch(API)
+
+	const products = await response.json()
+	displayProducts(products)
+}
+
+
+function displayProducts(products) {
+	const productList = document.querySelector('.main')
+
+	productList.innerHTML = products.map(product => `
+		<div class='product'>
+			<img src="${product.img}" alt='Error :(' class='product__img' />
+			<h2 class='product__title'>${product.title}</h2>
+			<p class='product__desc'>${product.description}</p>
+			<p class='product__price'>Цена: <span class='product__price_text'>${product.price}</span> сом</p>
+			<a href="update.html?id=${product.id}" class="product__edit">Редактировать</a>
+		</div>
+	`)
+}
+
+fetchProduct()
+
