4a63f6a0b7e6bdb811e36923b255b0e4cc5efbdb
Дастан <nuragzyuuludastan@gmail.com>
2024-11-27 10:29:16 +0600

    add function search

--- app.js
+++ app.js
@@ -17,10 +17,34 @@ function displayProducts(products) {
 			<h2 class='product__title'>${product.title}</h2>
 			<p class='product__desc'>${product.description}</p>
 			<p class='product__price'>Цена: <span class='product__price_text'>${product.price}</span> сом</p>
-			<a href='edit.html?id=${product.id}' class='edit__link'>Редактирование</a>
+			<div class='btns'>
+				<a href='edit.html?id=${product.id}' class='edit__link'>Редактирование</a>
+				<button class='delete__btn' onclick='deleteProduct(${product.id})'>Delete</button>
+			</div>
 		</div>
-	`)
+	`).join('')
+}
+
+
+async function deleteProduct(id) {
+	await fetch(`${API}/${id}`, {
+		method: 'DELETE'
+	})
+	fetchProduct()
 }
 
 fetchProduct()
 
+
+async function searchProduct(event) {
+	const searchQuery = event.target.value.toLowerCase()
+
+	const response = await fetch(API)
+	const products = await response.json()
+
+	const filteredProducts = products.filter(product => product.title.toLowerCase().includes(searchQuery))	
+
+	displayProducts(filteredProducts)
+}
+
+document.getElementById('search').addEventListener('input', searchProduct)
